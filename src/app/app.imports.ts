eb432834809ab489cfddbbf8cf2c8789fcbd5790
Koslun <adam.wamai@gmail.com>
2016-11-29 21:30:48 +0100

    feat(app.imports): import angulartics2 with ga provider
    Compiling this with "yarn run compile" shows that this does not compile with ngc, i.e. AoT no longer works.
    
    In comparison running "yarn start" works as it runs in JiT mode.

--- src/app/app.imports.ts
+++ src/app/app.imports.ts
@@ -1,10 +1,12 @@
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, PreloadAllModules } from '@angular/router';
+import { Angulartics2GoogleAnalytics, Angulartics2Module } from 'angulartics2';
 
 import { routes } from './app.routing';
 
 export const APP_IMPORTS = [
   ReactiveFormsModule,
-  RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+  Angulartics2Module.forRoot([Angulartics2GoogleAnalytics]),
 ];
 
